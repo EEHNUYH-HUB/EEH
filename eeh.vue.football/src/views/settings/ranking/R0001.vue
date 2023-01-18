352fd5ed020a30b1b3bc02a997083679704f3b9e
EEH <eehnuyh1@gmail.com>
2023-01-18 17:29:57 +0900

    2023-01-18

--- eeh.vue.football/src/views/settings/ranking/R0001.vue
+++ eeh.vue.football/src/views/settings/ranking/R0001.vue
@@ -4,7 +4,7 @@
             <Breadcrumb></Breadcrumb>
             <n-grid cols="1 s:1 m:2 l:2 xl:4 2xl:4" :x-gap="12" :y-gap="8" responsive="screen">
                 <n-gi v-for="(item, index) in data" :key="index">
-                    <n-card :title="item.title">
+                    <n-card :title="item.title" :id="'id'+item.type">
                         <n-data-table :columns="item.columns" :data="item.data" :bordered="true" :single-line="false"
                             single-column size="small" />
                     </n-card>
@@ -12,6 +12,7 @@
 
             </n-grid>
         </div>
+        <Anchor :Items="AnchorItems" DivID=".document-scroll-container"></Anchor>
     </div>
 
 
@@ -23,9 +24,11 @@ import { useStore } from "vuex";
 
 import Breadcrumb from "@/zenc/layout/components/Breadcrumb.vue"
 import { CashOutline as CashIcon } from '@vicons/ionicons5'
+import Anchor from "@/zenc/layout/components/Anchor.vue"
 
 const store = useStore()
 const data = ref(null)
+const AnchorItems = ref(new Array);
 
 onMounted(async () => {
     var dt = await store.state.apiClient.ExecDataTable('SQL', 'SELRANKING', null);
@@ -36,12 +39,19 @@ onMounted(async () => {
     data.value.push(convertTo('save',dt));
     data.value.push(convertTo('score',dt));
 
+    AnchorItems.value = new Array;
+    AnchorItems.value.push({ Title: '골 순위', ID: '#idgoal' });
+    AnchorItems.value.push({ Title: '도움 순위', ID: '#idassist' });
+    AnchorItems.value.push({ Title: '수비 순위', ID: '#idsave' });
+    AnchorItems.value.push({ Title: '도움 순위', ID: '#idscore' });
+
 
 })
 
 
 const convertTo = (type, data) => {
     var rtn = new Object;
+    rtn.type = type;
     rtn.columns = new Array;
     rtn.columns.push({ title: '순위', key: 'ranking' ,width: 50,align: 'center'})
     rtn.columns.push({ title: '이름', key: 'col_name'   })
